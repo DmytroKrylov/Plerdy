633bbcca16c793a0528a32b4564794d71de60add
Dmytro_Krylov <dmutrokrulov@gmail.com>
2024-02-07 11:42:52 +0200

    img

--- js/swiper.js
+++ js/swiper.js
@@ -1,6 +1,11 @@
     const swiper = new Swiper(".mySwiper", {
       slidesPerView: 1,
       spaceBetween: 20,
+      loop: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
       pagination: {
         el: ".swiper-pagination",
         clickable: true,
@@ -11,10 +16,14 @@
           spaceBetween: 20,
         },
         768: {
-          slidesPerView: 4,
+          slidesPerView: 3,
           spaceBetween: 20,
         },
         1024: {
+          slidesPerView: 4,
+          spaceBetween: 20,
+        },
+        1440: {
           slidesPerView: 5,
           spaceBetween: 20,
         },
