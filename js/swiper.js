1741d5ec6ebcb23ecfd1f7cd44eeaca7030db21f
Dmytro_Krylov <dmutrokrulov@gmail.com>
2024-02-07 15:00:50 +0200

    90%

--- js/swiper.js
+++ js/swiper.js
@@ -2,9 +2,13 @@
       slidesPerView: 1,
       spaceBetween: 20,
       loop: true,
-      autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
+      // autoplay: {
+      //   delay: 2500,
+      //   disableOnInteraction: false,
+      // },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
       },
       pagination: {
         el: ".swiper-pagination",
